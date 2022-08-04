173443e3eb02b7e38b8e9451b6bbf655330d8861
Mikhail <mvnorkin@gmail.com>
2022-08-04 11:11:13 +0300

    Task61 complite

--- Task61/Program.cs
+++ Task61/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3
+*/
+
+Console.Write($"Сколько чисел вы хотети ввести?. Введите колчиество: ");
+int a = 0;
+
+a = int.Parse(Console.ReadLine());
+
+int sum = 0;
+int[] array = new int [a];
+for (int i = 0; i < a; i++)
+{
+    Console.Write($"Введите {i+1} число:");
+    array[i] = int.Parse(Console.ReadLine());
+    if (array[i] > 0)
+    {
+        sum = sum + 1;
+    }
+}
+
+for (int i =0; i < a; i++)
+{
+    Console.Write($"{array[i]} ");
+}
+Console.WriteLine($"-> {sum}");
